7fd536f0ad05f072e795aebd2412f3da26d732fa
Oscar A. <oscar_alhuay2001@hotmail.com>
2026-07-26 18:02:48 -0300

    feat: add FirstPage1 product management exercise

---
+++ junior2/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FirstPage1 from "./components/FirstPage1.jsx";
+
+function App({number}) {
+  return (
+    <div className="app">
+      <FirstPage1/>
+    </div>
+  );
+}
+
+export default App;
